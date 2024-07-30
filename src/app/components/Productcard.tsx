2e1995f05b31d02123e20e1612f912813778f9ea
JayaniAththanagoda <165479877+JayaniAththanagoda@users.noreply.github.com>
2024-07-30 17:47:00 +0530

    next.js-basics

---
+++ src/app/components/Productcard.tsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import AddToCart from "./AddToCart";
+
+
+
+const Productcard = () => {
+    return(
+        <div >
+           <AddToCart />
+        </div>
+    )
+}
+export default Productcard
